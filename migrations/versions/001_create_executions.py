234aa194e403829211e2d920a078773b9ea3a732
Tarek76578 <168303910+Tarek76578@users.noreply.github.com>
2026-09-05 21:29:31 +0100

    fix: sort migration imports

--- migrations/versions/001_create_executions.py
+++ migrations/versions/001_create_executions.py
@@ -4,8 +4,8 @@ Revision ID: 001_create_executions
 Revises:
 """
 
-from alembic import op
 import sqlalchemy as sa
+from alembic import op
 from sqlalchemy.dialects import postgresql
 
 revision = "001_create_executions"
